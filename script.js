e04a7ee456971473578825c41c2827f1e028fbe4
ayanokoji <lambrecht.daniel@escola.pr.gov.br>
2025-04-15 07:41:08 -0300

    aa

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function atualizarRelogio() {
-    const relogio = document.getElementById('relogio')
-   const agora = new Date();
-   agora.setHours(agora.getHours()-3)
-   cont horas = String(agora.getHours()).padStart(2,'0')
-   cont minutos = String(agora.getMinutes())pedStart(2,'0')
-   cont segundos = String(agora.getMinutes())pedStart(2,'0')
-   relogioElement.textContent = `${horas}` `${minutos}``${segundos}`
-}
+function hora(){
+    let relogio = document.getElementById('relogio')
+
+    let tempo = new Date();
+    let horas = tempo.getHours();
+    let minutos = tempo.getMinutes();
+    let segundos = tempo.getSeconds();
+
+    relogio.innerHTML = `${horas}:${minutos}:${segundos}` 
+    
+
+
+  }
+setInterval(hora,1000)
+
+  hora();
+
+  
